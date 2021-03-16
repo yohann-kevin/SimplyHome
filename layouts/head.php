e423d80e9fbf589ba7465f90a3b0e017fb0710a8
Kirua <yo44prg@icloud.com>
2021-03-16 21:57:09 +0100

    feat : add header

--- layouts/head.php
+++ layouts/head.php
@@ -8,5 +8,5 @@
     <link rel="StyleSheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <link rel="StyleSheet" href="./fonts/stylesheet.css">
     <link rel="StyleSheet" href="./style/main.css">
-    <title>Document</title>
+    <title>Simply Home</title>
 </head>
--- layouts/header.php
+++ layouts/header.php
@@ -8,7 +8,14 @@
                 <li><a href="#"><i class="fab fa-instagram"></i></a></li>
             </ul>
 
-            <a href="#"><i class="fas fa-phone"></i>02 57 87 71 85</a>
-            <a href="#"><i class="fas fa-home"></i>Suivre mon projet</a>
+            <a href="#"><i class="fas fa-phone"></i> 02 57 87 71 85</a>
+            <a href="#"><i class="fas fa-home"></i> Suivre mon projet</a>
+        </div>
+        <div id="containerHeader">
+            <div id="headerBackImg">
+                <img src="images/calypso_3_chambres.jpg" alt="maison 3 chambres">
+            </div>
+            <div id="blackBackImg"></div>
+            <nav id="navbar"></nav>
         </div>
     </header>
