20e22c65596ac38b65dcc32b7f160cf2da51e390
Kirua <yo44prg@icloud.com>
2021-03-17 16:12:57 +0100

    feat : modify minifier

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php  
+<?php
+$page = "home";
+$title = "Bienvenue";
 include_once './layouts/head.php';
 include_once './layouts/header.php';
 ?>
--- script/cssMinifier.js
+++ script/cssMinifier.js
@@ -1,14 +1,20 @@
 let fs = require('fs');
 let uglifycss = require('uglifycss');
 
-let uglified = uglifycss.processFiles(
+let minifiedFonts = uglifycss.processFiles(
+    [ "fonts/stylesheet.css"],
+    { maxLineLen: 500 }
+);
+
+let minifiedMain = uglifycss.processFiles(
     [ "style/main.css"],
     { maxLineLen: 500 }
 );
 
+fs.unlinkSync("fonts/stylesheet.css");
 fs.unlinkSync("style/main.css");
 
-fs.writeFile("style/main.css", uglified, function(err) {
+fs.writeFile("fonts/stylesheet.css", minifiedFonts, function(err) {
     if(err) {
         console.log("Error : " + err);
     } else {
@@ -16,5 +22,10 @@ fs.writeFile("style/main.css", uglified, function(err) {
     }
 })
 
-console.log(fs);
-console.log(uglified);
+fs.writeFile("style/main.css", minifiedMain, function(err) {
+    if(err) {
+        console.log("Error : " + err);
+    } else {
+        console.log("This files is minified ! ");
+    }
+})
