4372a3f59e15c80550632790d212b78c1893a9f8
Kirua <yo44prg@icloud.com>
2021-03-19 09:16:49 +0100

    feat : add content in page about

--- about.php
+++ about.php
@@ -26,17 +26,89 @@ include_once 'layouts/header.php';
     <section id="sectionAbout" class="maxLength">
         <article class="articleAbout">
             <h3 onclick="accordion(this)"><i class="fas fa-chevron-circle-down"></i> Nos engagements et garanties</h3>
-            <div class="articleAboutContent"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, voluptates?</p></div>
+            <div class="articleAboutContent">
+                <div class="articleAboutPara">
+                    <p>Nous mettons tout en œuvre pour vous offrir une maison qui réponde en tous points à vos
+                        exigences. Ainsi, nous nous engageons à respecter les délais que nous nous sommes fixés,
+                        en ne travaillant qu’avec des matériaux de qualité et adaptés. De plus, nous respectons
+                        toutes les normes et les réglementations, notamment les normes RT 2012, ainsi que le
+                        Contrat de Construction de Maison Individuelle.</p><br>
+                    <p>Nous vous invitons à découvrir l’ensemble de nos garanties : </p>
+                </div>
+                <div class="articleImg">
+                    <ul>
+                        <li>Garantie de remboursement d’acompte</li>
+                        <li>Garantie de livraison à prix et délai convenus</li>
+                        <li>Assurance dommages-ouvrage</li>
+                        <li>Garantie de parfait achèvement</li>
+                        <li>Garanties biennale et décennale</li>
+                        <li>Garantie de bon fonctionnement</li>
+                        <li>Paiement des travaux strictement réglementé</li>
+                    </ul>
+                    <figure class="imgArticleAbout">
+                        <img src="./images/pa-picto-01.png" alt="simply home">
+                    </figure>
+
+                </div>
+            </div>
         </article>
 
         <article class="articleAbout">
             <h3 onclick="accordion(this)"><i class="fas fa-chevron-circle-right"></i> Nos engagements et garanties</h3>
-            <div class="articleAboutContent"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, voluptates?</p></div>
+            <div class="articleAboutContent">
+                <div class="articleAboutPara">
+                    <p>Nous mettons tout en œuvre pour vous offrir une maison qui réponde en tous points à vos
+                        exigences. Ainsi, nous nous engageons à respecter les délais que nous nous sommes fixés,
+                        en ne travaillant qu’avec des matériaux de qualité et adaptés. De plus, nous respectons
+                        toutes les normes et les réglementations, notamment les normes RT 2012, ainsi que le
+                        Contrat de Construction de Maison Individuelle.</p><br>
+                    <p>Nous vous invitons à découvrir l’ensemble de nos garanties : </p>
+                </div>
+                <div class="articleImg">
+                    <ul>
+                        <li>Garantie de remboursement d’acompte</li>
+                        <li>Garantie de livraison à prix et délai convenus</li>
+                        <li>Assurance dommages-ouvrage</li>
+                        <li>Garantie de parfait achèvement</li>
+                        <li>Garanties biennale et décennale</li>
+                        <li>Garantie de bon fonctionnement</li>
+                        <li>Paiement des travaux strictement réglementé</li>
+                    </ul>
+                    <figure class="imgArticleAbout">
+                        <img src="./images/pa-picto-01.png" alt="simply home">
+                    </figure>
+
+                </div>
+            </div>
         </article>
 
         <article class="articleAbout">
             <h3 onclick="accordion(this)"><i class="fas fa-chevron-circle-right"></i> Nos engagements et garanties</h3>
-            <div class="articleAboutContent"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, voluptates?</p></div>
+            <div class="articleAboutContent">
+                <div class="articleAboutPara">
+                    <p>Nous mettons tout en œuvre pour vous offrir une maison qui réponde en tous points à vos
+                        exigences. Ainsi, nous nous engageons à respecter les délais que nous nous sommes fixés,
+                        en ne travaillant qu’avec des matériaux de qualité et adaptés. De plus, nous respectons
+                        toutes les normes et les réglementations, notamment les normes RT 2012, ainsi que le
+                        Contrat de Construction de Maison Individuelle.</p><br>
+                    <p>Nous vous invitons à découvrir l’ensemble de nos garanties : </p>
+                </div>
+                <div class="articleImg">
+                    <ul>
+                        <li>Garantie de remboursement d’acompte</li>
+                        <li>Garantie de livraison à prix et délai convenus</li>
+                        <li>Assurance dommages-ouvrage</li>
+                        <li>Garantie de parfait achèvement</li>
+                        <li>Garanties biennale et décennale</li>
+                        <li>Garantie de bon fonctionnement</li>
+                        <li>Paiement des travaux strictement réglementé</li>
+                    </ul>
+                    <figure class="imgArticleAbout">
+                        <img src="./images/pa-picto-01.png" alt="simply home">
+                    </figure>
+
+                </div>
+            </div>
         </article>
     </section>
 </main>
