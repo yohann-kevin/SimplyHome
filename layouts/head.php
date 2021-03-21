b313450353a5d754096575ae3695d2abab49413a
Kirua <yo44prg@icloud.com>
2021-03-21 22:04:04 +0100

    feat : add maquette xd

--- layouts/head.php
+++ layouts/head.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="StyleSheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-    <link rel="StyleSheet" href="./fonts/stylesheet.css">
     <link rel="StyleSheet" href="./style/main.css">
     <link rel="stylesheet" href="./style/mediaqueries.css">
     <title>Simply Home | <?= $title ?></title>
